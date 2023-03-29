8f75d3367fce8110eebcb4330134a873b50ed60b
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-03-29 17:48:52 +0900

    feat: create App.js and three components: Map, Memories and PopupMenu

---
+++ ken-ken-pa/src/components/Map.jsx
@@ -0,0 +1,13 @@
+//this Map.jsx file is for tentative use. It's replaced by Map.jsx Joe created.
+
+import React from "react";
+
+export default function Map(props) {
+    const {onClick} = props;
+    
+    return(
+    <div onClick={onClick}>
+        This is a tentative Japanese Map!
+    </div>
+   );
+}
