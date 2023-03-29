ab48f918c77761ea02017b18243a3a9250f5a462
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-03-29 19:37:35 +0900

    fix: rebase from feature branch

--- ken-ken-pa/src/components/Map.jsx
+++ ken-ken-pa/src/components/Map.jsx
@@ -1,13 +1,24 @@
-//this Map.jsx file is for tentative use. It's replaced by Map.jsx Joe created.
-
 import React from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import prefectures from "../data/japan_prefectural_borders.json";
 
-export default function Map(props) {
-    const {onClick} = props;
-    
-    return(
-    <div onClick={onClick}>
-        This is a tentative Japanese Map!
-    </div>
-   );
-}
+export default function Map() {
+    return (
+        <div>
+            <ComposableMap
+                projection="geoMercator"
+                projectionConfig={{
+                    center: [135, 35],
+                    scale: 1000
+                }}>
+                <Geographies geography={prefectures}>
+                {({ geographies }) =>
+                    geographies.map((geo) => (
+                    <Geography key={geo.rsmKey} geography={geo} />
+                    ))
+                }
+                </Geographies>
+            </ComposableMap>
+        </div>
+    );
+};
