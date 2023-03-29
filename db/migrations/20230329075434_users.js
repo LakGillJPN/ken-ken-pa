1afe433fa7bd3950baca1d652f11de8228454ccb
LakGillJPN <lakgill88@gmail.com>
2023-03-29 17:52:23 +0900

    feat: make first migration

---
+++ db/migrations/20230329075434_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('user', function(table) {
+    table.increments('id')
+    .primary();
+    table.varchar('username').
+    unique()
+    .notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  knex.schema.dropTable("user");
+};
