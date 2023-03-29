f09c6d67850b62e40b3dc45439daacce3a3a0355
LakGillJPN <lakgill88@gmail.com>
2023-03-29 18:48:00 +0900

    feat: "make user_has_prefecture table"

--- db/migrations/20230329085121_user_has_prefecture.js
+++ db/migrations/20230329085121_user_has_prefecture.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('user_has_prefecture', function(table) {
+    table.integer('user_id')
+    .references('id')
+    .inTable('user');
+    table.integer('prefecture_id')
+    .notNullable();
+  })
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('user_has_prefecture');
 };
