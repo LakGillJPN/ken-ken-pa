5e41f4988dbd59d016fc41828c174fa8a831ed50
LakGillJPN <lakgill88@gmail.com>
2023-03-30 21:02:10 +0900

    feat add seeds for users and photos

--- db/seeds/002-users.js
+++ db/seeds/002-users.js
@@ -5,11 +5,11 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
-  await knex('table_name').insert([
-    {id: 1, email_address: 'hoge1234@hotmail.com'},
-    {id: 2, email_address: 'pushman@gmail.com'},
-    {id: 3, email_address: 'inthepast@aol.com'},
-    {id: 4, email_address: 'gitignore@gmail.com'},
-    {id: 5, email_address: 'npmall@docomo.com'},
+  await knex('users').insert([
+    {id: 1,  email: 'hoge1234@hotmail.com'},
+    {id: 2,  email: 'pushman@gmail.com'},
+    {id: 3,  email: 'inthepast@aol.com'},
+    {id: 4,  email: 'gitignore@gmail.com'},
+    {id: 5,  email: 'npmall@docomo.com'},
   ]);
 };
