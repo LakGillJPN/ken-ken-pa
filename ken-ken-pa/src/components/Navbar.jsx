1bcbfc14ac5214135a1af769ae1bade5198f4b65
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-03-30 21:25:46 +0900

    feat: [wip] make my memories button in Navbar

--- ken-ken-pa/src/components/Navbar.jsx
+++ ken-ken-pa/src/components/Navbar.jsx
@@ -5,13 +5,17 @@ export default function Navbar(props) {
 
     return (
     <div className="navbar">
-        { currentView !== "" ? (
+        <h2>Logout</h2>
+
+        { currentView === "Memories" ? (
             <h1 
             className="home-button"
             onClick={goToHome}>Home
             </h1>
-        ) : ("")}
-        
+        ) : (
+            <button>My Memories</button>
+        )}
+
         <div>
             <button onClick={changeLangToJa}>日本語</button>
             <button onClick={changeLangToEn}>English</button>
