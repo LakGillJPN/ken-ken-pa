908a70abf9ac49ac88c0d4fa4c88857a8a657bb5
TaigaShiga <gaitatg97@gmail.com>
2023-04-01 00:05:09 +0200

    feat: login to firebase

--- server/firebase/auth.js
+++ server/firebase/auth.js
@@ -1,29 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const firebase = require("firebase/app");
+const {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} = require("firebase/auth");
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAUhr2UaUt6CwNUwIW8boTW9mV7DWQ3xTk",
   authDomain: "ccp7-greenfiled.firebaseapp.com",
   projectId: "ccp7-greenfiled",
   storageBucket: "ccp7-greenfiled.appspot.com",
   messagingSenderId: "103434290490",
-  appId: "1:103434290490:web:0f6a8f6dba1d3aa552bc03",
-  measurementId: "G-JMKWPXEX7V"
+  appId: "1:103434290490:web:76052eefd21135ad52bc03",
+  measurementId: "G-11GFF02X0T"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
 
-export async function loginWithEmailandPassword(email, password) {
-  try {
-    const user = await signInWithEmailAndPassword(auth, email, password);
-    console.log("Login Success");
-    return user;
-  } catch (err) { 
-    console.log("ERROR🔥", err);
+const auth = getAuth();
+
+module.exports = {
+  // login
+  loginWithEmailAndPassword: async function (email, password) {
+    try {
+      let user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+      return user;
+    } catch (err) {
+      console.log("ERROR🔥", err);
+      return undefined;
+    }
   }
 }
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { loginWithEmailandPassword } = require("../firebase/auth");
+const { loginWithEmailAndPassword } = require("../firebase/auth");
 
 router.post("/login", async(req, res) => {
-  console.log("request.body is", req.body);
-  // user email and password
   const { email, password } = req.body;
-  // invoke authentication
-  const user = await loginWithEmailandPassword(email, password);
-  console.log("user", user);
-  res.status(200).send(user);
+  
+  const user = await loginWithEmailAndPassword(email, password);
+  // console.log("🐳", email===user.user.email);
+  res.status(200).send(user.user);
 });
 
 // export 
