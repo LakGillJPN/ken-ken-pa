5ff5bd37355e53c671940eb2e5e4cc9b7b94ad19
kappanjoe <6261485+kappanjoe@users.noreply.github.com>
2023-04-01 11:26:04 +0900

    feat: add localization to Login and SubmitBtn

--- ken-ken-pa/src/components/SubmitBtn.jsx
+++ ken-ken-pa/src/components/SubmitBtn.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import UIText from "../data/locales.json";
 import "./SubmitBtn.css"
 
 // submit btn component
 export default function SubmitBtn(props) {
-  return (<button className='btn' type='submit' value="Submit">Submit</button>);
+  const { type, currentLocale } = props;
+  var labelText;
+  switch (type) {
+    case "signup":
+      labelText = UIText['signup-submit'][currentLocale];
+      break;
+    case "login":
+      labelText = UIText['login-submit'][currentLocale];
+      break;
+    case "memory":
+      labelText = UIText['submit-memory'][currentLocale];
+      break;
+    default:
+      labelText = "Submit";
+      break;
+  }
+
+  return (<button className='btn' type='submit' value="Submit">{labelText}</button>);
 }
