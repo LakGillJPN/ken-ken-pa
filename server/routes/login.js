9adf037e76a31ebffda736160245a82449bd58f8
TaigaShiga <gaitatg97@gmail.com>
2023-04-01 08:19:12 +0200

    feat: make jwt token file

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { loginWithEmailAndPassword } = require("../firebase/auth");
+ 
 
 router.post("/login", async(req, res) => {
   const { email, password } = req.body;
   const user = await loginWithEmailAndPassword(email, password);
+  //
+  console.log(user.user.reloadUserInfo.localId);
   
   if (user === undefined) {
     // login unsuccess
